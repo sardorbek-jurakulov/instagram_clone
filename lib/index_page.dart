e054a603b982829cf9db65b708a60b0026b3f42b
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-09-09 17:27:04 +0500

    appBar is ready

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({Key? key}) : super(key: key);
@@ -13,7 +14,31 @@ class _IndexPageState extends State<IndexPage> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          leading: Image.asset("assets/images/instagram_text_logo.png"),
+          leadingWidth: 120,
+          leading: Container(
+            margin: EdgeInsets.only(
+              left: 10,
+            ),
+            child: Image.asset(
+              "assets/images/instagram_text_logo.png",
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.plus_app, color: Colors.black),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.heart, color: Colors.black),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
+            ),
+          ],
+          backgroundColor: Colors.white,
+          shadowColor: Colors.transparent,
         ),
         body: Center(
           child: Text("Instagram Clone"),

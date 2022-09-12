d91f531fceaaf5f4a89853be519cf9b405102990
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-09-12 13:50:13 +0500

    WIP

---
+++ lib/models/user_post_model.dart
@@ -0,0 +1,7 @@
+class UserPost {
+  String authorUsername;
+  List<String> graphicsPaths;
+  int likesCount;
+  String text;
+  UserPost(this.authorUsername, this.graphicsPaths, this.likesCount, this.text);
+}

d144fc149893549b22528eea37ee8995a38e9e38
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-09-14 12:09:42 +0500

    WIP

---
+++ lib/DataBase/posts_storage.dart
@@ -0,0 +1,10 @@
+import 'package:instagram_clone/models/user_post_model.dart';
+
+List<UserPost> postsStorage = [
+  UserPost("zarifaka", ["assets/images/third_avatar.jpeg"], 0,
+      "Assalamu alaykum hurmatli robotiks lab o'quvchilari va abuturientlari. Nima gap silani yanig kursimizga taklif qilamiz. Karuchi kursni biri, c++ da algaritmlash karochi. Bilsela bilarsila, darsni kaminani o'zi o'tadi. Yengil telefon qilib ro'yxatdan o'tovrasila. Sila bilan Zarif brat"),
+  UserPost("zarifaka", ["assets/images/third_avatar.jpeg"], 0,
+      "Assalamu alaykum hurmatli robotiks lab o'quvchilari va abuturientlari. Nima gap silani yanig kursimizga taklif qilamiz. Karuchi kursni biri, c++ da algaritmlash karochi. Bilsela bilarsila, darsni kaminani o'zi o'tadi. Yengil telefon qilib ro'yxatdan o'tovrasila. Sila bilan Zarif brat"),
+  UserPost("zarifaka", ["assets/images/third_avatar.jpeg"], 0,
+      "Assalamu alaykum hurmatli robotiks lab o'quvchilari va abuturientlari. Nima gap silani yanig kursimizga taklif qilamiz. Karuchi kursni biri, c++ da algaritmlash karochi. Bilsela bilarsila, darsni kaminani o'zi o'tadi. Yengil telefon qilib ro'yxatdan o'tovrasila. Sila bilan Zarif brat"),
+];

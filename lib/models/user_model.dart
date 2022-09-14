c64ec2b08aff99333865dc3763672f8cc63e8373
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-09-14 21:22:23 +0500

    WIP

---
+++ lib/models/user_model.dart
@@ -0,0 +1,9 @@
+class User {
+  String firstName;
+  String secondName;
+  String middleName;
+  String username;
+  String avatarPath;
+  User(this.firstName, this.secondName, this.middleName, this.username,
+      this.avatarPath);
+}

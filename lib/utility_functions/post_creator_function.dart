4bf9cb7ccca7c998578cda44727c8770c301de54
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-09-14 12:28:41 +0500

    WIp

---
+++ lib/utility_functions/post_creator_function.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PostCreator extends StatelessWidget {
+  const PostCreator({Key? key}) : super(key: key);
+
+  Widget postCreator() {
+    return Text("Post creator function is work");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return postCreator();
+  }
+}
